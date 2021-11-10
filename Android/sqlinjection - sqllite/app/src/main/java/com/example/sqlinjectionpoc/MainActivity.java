c07a727dcd6b35c062066115bbcd5ae08d024828
AndreMaquesMachado <84318030+AndreMaquesMachado@users.noreply.github.com>
2021-11-10 16:23:40 +0000

    add update test

---
+++ Android/sqlinjection - sqllite/app/src/main/java/com/example/sqlinjectionpoc/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.sqlinjectionpoc;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText input;
+    TextView showInput;
+
+    DatabaseHelper dbhelper;
+    SQLiteDatabase db;
+    public static final String TB_NAME = "usertable";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.editText);
+        showInput = (TextView) findViewById(R.id.textView2);
+        dbhelper = new DatabaseHelper(this, TB_NAME, null, 1);
+        db = dbhelper.getWritableDatabase();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_1:
+                showResult(input.getText().toString());
+                break;
+        }
+    }
+
+    public void showResult(String info) {
+        if (info == null || info.length() <= 0)
+            showInput.setText("Please input:");
+        else {
+            Cursor cursor;
+            //Testing raw query - exposed to 1' or '1'='1
+            //cursor = db.rawQuery("SELECT * FROM usertable WHERE _id='" + info + "'", null);
+            //Testing raw query with parameterized input - failed to 1' or '1'='1
+            //String m_argv[] = {input.getText().toString()};
+            //cursor = db.rawQuery("SELECT * FROM  usertable WHERE _id=?", m_argv);
+            String m_argv[] = {input.getText().toString()};
+            ContentValues cv = new ContentValues();
+            cv.put("username","male");
+            //cv.put("password","1; Select * from usertable;");
+            cv.put("password","delete from usertable where _id =2");
+            db.update("usertable",cv,"_id=?",new String[]{String.valueOf(1)});
+            cursor = db.rawQuery("SELECT * FROM  usertable WHERE _id='" + info + "'", null);
+            cursor.moveToFirst();
+            String result = "";
+            while (!cursor.isAfterLast()) {
+                result += "id:" + cursor.getInt(0) + "\r\n" + "user:" + cursor.getString(1) + "\r\n" + "pass:" + cursor.getString(2) + "\r\n";
+                cursor.moveToNext();
+            }
+            showInput.setText(result);
+            cursor.close();
+        }
+    }
+}
